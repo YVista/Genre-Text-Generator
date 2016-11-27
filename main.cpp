acc2447587b72a91a04ecd73b95b62288d18eaf3
YVista <y.vista@hotmail.com>
2016-11-27 04:41:45 +0000

    ver. 0.3 古风白学模拟器
    从处理双音节、四音节词的古风模拟器，升级为可以处理单音节、双音节、四音节的古风白学模拟器。

--- main.cpp
+++ main.cpp
@@ -13,16 +13,18 @@
 using namespace std;
 
 #define ARR_LEN(array, length){ length =  sizeof(array) / sizeof(array[0]); }
+string ch1[]={"爱","恨","等","哭"};
 string ch2[]={"雪花","黄沙","天涯","蒹葭","朱砂","琵琶","琅琊","狼牙","篱笆","悬崖","大侠","寒鸦","枝桠","喑哑"};
-string ch4[]={"负了天下","一场繁华","泪如雨下","刀剑喑哑","还就菊花","不可自拔","红了桃花"};
-string sen[]={"xx，xx，许你xxxx","xx，xx，怎敌你xxxx","xx，xx，只想再见你xxxx","xx，xx，我今生xxxx"};
+string ch4[]={"负了天下","一场繁华","泪如雨下","刀剑喑哑","还就菊花","不可自拔","红了桃花","眉目如画"};
+string sen[]={"xxxx什么的已经无所谓了。因为已经不再有xx，值得去x了。","传达不了的xx已经不需要了。因为已经不再有xx，值得去x了。","我即使是死了，钉在棺材里了，也要在墓里，用这腐朽的声带喊出：xxxx！","为什么你会这么xx啊！你xxxx过多少次了啊！？你到底要多xxxx才甘心啊！？"};
 int main()
-{       int ch2_len, ch4_len, sen_len;
-        string ch2_ran, ch4_ran, sen_ran;
+{       int ch1_len, ch2_len, ch4_len, sen_len;
+        string ch1_ran, ch2_ran, ch4_ran, sen_ran;
+        ARR_LEN(ch1, ch1_len);
         ARR_LEN(ch2, ch2_len);
         ARR_LEN(ch4, ch4_len);
         ARR_LEN(sen, sen_len);
-        int sen_t, ch2_t, ch4_t;
+        int sen_t, ch1_t, ch2_t, ch4_t;
     cout<<"按回车键继续生成，按其他键加回车退出"<<endl;
     while( getchar()=='\n')
     {   srand(time(0));       // 种子
@@ -41,6 +43,13 @@ int main()
             ch2_ran=ch2[ch2_t]; // ch2中的随机项
             sen_ran = sen_ran.replace(sen_ran.find("xx"), 2, ch2_ran); //从第一个xx位置替换第一个xx为ch2
             pos_ch2=sen_ran.find("xx");
+        }
+        string::size_type pos_ch1=sen_ran.find("x");
+        while (pos_ch1 != string::npos)
+        {   ch1_t = rand() % ch1_len;       // 给ch1的随机序数
+            ch1_ran=ch1[ch1_t]; // ch1中的随机项
+            sen_ran = sen_ran.replace(sen_ran.find("x"), 1, ch1_ran); //从第一个x位置替换第一个x为ch1
+            pos_ch1=sen_ran.find("x");
         }
         cout << sen_ran << endl;
     }
