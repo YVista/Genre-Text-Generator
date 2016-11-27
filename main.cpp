2bb85f9d49e944d2a19f1fc9b3419b9f2d4590d6
Y. Vista <y.vista@hotmail.com>
2016-11-27 05:26:45 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,21 +10,18 @@
 #include <ctime>
 #include <cstdlib>
 #include <stdio.h>
+#include "gufeng baixue.h"
 using namespace std;
 
 #define ARR_LEN(array, length){ length =  sizeof(array) / sizeof(array[0]); }
-string ch1[]={"爱","恨","等","哭"};
-string ch2[]={"雪花","黄沙","天涯","蒹葭","朱砂","琵琶","琅琊","狼牙","篱笆","悬崖","大侠","寒鸦","枝桠","喑哑"};
-string ch4[]={"负了天下","一场繁华","泪如雨下","刀剑喑哑","还就菊花","不可自拔","红了桃花","眉目如画"};
-string sen[]={"xxxx什么的已经无所谓了。因为已经不再有xx，值得去x了。","传达不了的xx已经不需要了。因为已经不再有xx，值得去x了。","我即使是死了，钉在棺材里了，也要在墓里，用这腐朽的声带喊出：xxxx！","为什么你会这么xx啊！你xxxx过多少次了啊！？你到底要多xxxx才甘心啊！？"};
 int main()
 {       int ch1_len, ch2_len, ch4_len, sen_len;
-        string ch1_ran, ch2_ran, ch4_ran, sen_ran;
-        ARR_LEN(ch1, ch1_len);
-        ARR_LEN(ch2, ch2_len);
-        ARR_LEN(ch4, ch4_len);
-        ARR_LEN(sen, sen_len);
-        int sen_t, ch1_t, ch2_t, ch4_t;
+    string ch1_ran, ch2_ran, ch4_ran, sen_ran;
+    ARR_LEN(ch1, ch1_len);
+    ARR_LEN(ch2, ch2_len);
+    ARR_LEN(ch4, ch4_len);
+    ARR_LEN(sen, sen_len);
+    int sen_t, ch1_t, ch2_t, ch4_t;
     cout<<"按回车键继续生成，按其他键加回车退出"<<endl;
     while( getchar()=='\n')
     {   srand(time(0));       // 种子
@@ -53,5 +50,5 @@ int main()
         }
         cout << sen_ran << endl;
     }
-        return 0;
+    return 0;
 }
