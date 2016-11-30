310c99b3b5e44c3e41144f56cd1ac3e71ead55d6
Y. Vista <y.vista@hotmail.com>
2016-11-30 00:34:08 +0000

    膜蛤句式

---
+++ moha_sen.h
@@ -0,0 +1,12 @@
+//
+//  moha_sen.h
+//  Genre Text Generator
+//
+//  Created by Y.Vista on 16/11/27.
+//  Copyright © 2016年 Y.Vista. All rights reserved.
+//
+
+using namespace std;
+
+string sen[]={"问我这个是不是有点钦定的意思，那我可以说一句xxxx","xx的哪一个xx我没去过","美国的华莱士比你们不知道高到哪里去了，我和他xxxx","你们啊，还要努力提高xx水平","那我是xxxx，见得多了","too x，too xx，sometimes xxxx","比xxxx跑得还快","中国有一句老话叫xx大xx","你还要多xx一个","如果xx有偏差，你们要xxxx的知道吧","xx也要按照香港的法律","我们的xx权也是很重要的","弄个大xx","xx也要按照基本法","我今天是小小地xx了你们一下","you mean I am xxxx?","那你们可另请xx吧","你们给我搞的这个东西啊，xxxx！","当时我就吟了两句诗：xxxxxxx，xxxxxxx","好几百个教授一致xxxx","人呐，固然要有个人的xx，还要考虑到历史的xx","中央都决定了，就让你来xxxx","很惭愧，就做了一点微小的xx，谢谢大家"};
+
